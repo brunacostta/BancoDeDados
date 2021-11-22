70ee1f9850902a34f4edfae8c6a58f1e25c0f5ec
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-11-22 12:06:29 -0300

    Correção Servlet produto

--- Atp53_JavaWeb/atp53/src/main/java/Produto.java
+++ Atp53_JavaWeb/atp53/src/main/java/Produto.java
@@ -2,11 +2,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-WebServlet(urlPatterns = "/produto")
+@WebServlet(urlPatterns = "/produto")
 public class Produto extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
