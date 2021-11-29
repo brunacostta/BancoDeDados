29a19e0977f4125871225ee1997b81f0600e5e6d
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-11-29 18:22:14 -0300

    ATP 65 - framework Spring, utilizando persistência, Web MVC, Thymeleaf e tambem o bootstrap.

--- ATP64/atp64/src/main/java/com/bruna/atp64/model/CadastroClienteModel.java
+++ ATP64/atp64/src/main/java/com/bruna/atp64/model/CadastroClienteModel.java
@@ -6,34 +6,24 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
     @Entity
     @Table(name = "cadastroCliente")
     public class CadastroClienteModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter 
+    @Setter
     private int id;
+
+    @Getter 
+    @Setter
     private String nome;
+
+    @Getter 
+    @Setter
     private String sobrenome;
-
-    public void setId(int id) {
-        this.id = id;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getNome() {
-        return nome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-        
+   
 }
