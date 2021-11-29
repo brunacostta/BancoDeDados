f392e843b55eff0ddb9554706f9eabae9b3e5b5a
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-11-29 15:09:45 -0300

    Finalização ATP 64

--- ATP64/atp64/src/main/java/com/bruna/atp64/controller/CadastroClientesController.java
+++ ATP64/atp64/src/main/java/com/bruna/atp64/controller/CadastroClientesController.java
@@ -9,6 +9,8 @@ import com.bruna.atp64.repository.CadastroClienteRepository;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CadastroClientesController {
@@ -27,11 +29,28 @@ public class CadastroClientesController {
     }
 
     @GetMapping("/cadastroCliente/form")
-    public String formulario(){
+    public String formulario(HttpServletRequest req){
+        CadastroClienteModel model = new CadastroClienteModel();
+        req.setAttribute("cadastro", model);
         return "cadastroCliente-form";
     }
 
-    public String salvar(){
-        return "redirect: /cadastroCliente";
+    @PostMapping("/cadastroCliente/salvar")
+    public String salvar(CadastroClienteModel model){
+        repository.save(model);
+        return "redirect:/cadastroCliente";
+    }
+
+    @GetMapping("/cadastroCliente/deletar/{id}")
+    public String deletar(@PathVariable int id){
+        repository.deleteById(id);
+        return "redirect:/cadastroCliente";
+    }
+
+    @GetMapping("/cadastroCliente/editar/{id}")
+    public String editar(@PathVariable int id, HttpServletRequest req){
+        CadastroClienteModel model = repository.findById(id).get();
+        req.setAttribute("cadastro", model);
+        return "cadastroCliente-form";
     }
 }
